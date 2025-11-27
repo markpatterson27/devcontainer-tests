f84ac10cc043b3db4bbc78f41c9d36f12ef12428
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2025-11-27 15:10:02 +0000

    fix: update postStartCommand and write-timestamp.sh to use the correct directory for timestamp files

--- .devcontainer/scripts/write-timestamp.sh
+++ .devcontainer/scripts/write-timestamp.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
 # Create timestamp file with timestamp
-TIMESTAMP_FILE="${1:-./.postcreate_ms}"
+
+TIMESTAMP_FILE="${1:-./.measure/postcreate_ms}"
+
+#create directory if it doesn't exist
+mkdir -p "$(dirname "$TIMESTAMP_FILE")"
+
 date +%s.%3N > "$TIMESTAMP_FILE"
 echo "Timestamp written to $TIMESTAMP_FILE"
